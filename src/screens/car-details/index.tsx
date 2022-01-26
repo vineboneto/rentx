601343988a4f072c4aee018f551e499125acade2
vineboneto <vineboneto@gmail.com>
2022-01-26 20:33:44 -0300

    feat: animated scroll view

--- src/screens/car-details/index.tsx
+++ src/screens/car-details/index.tsx
@@ -43,7 +43,6 @@ export function CarDetails() {
   const scrollY = useSharedValue(0)
   const scrollHandler = useAnimatedScrollHandler((event) => {
     scrollY.value = event.contentOffset.y
-    console.log(event.contentOffset.y)
   })
   const headerStyleAnimation = useAnimatedStyle(() => {
     return {
